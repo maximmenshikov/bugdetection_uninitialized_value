a02aab5aa47b6488c285a480cb6ee66b0ed215af
Maxim Menshchikov <maximmenshchikov@gmail.com>
2016-11-23 12:43:16 +0300

    Add a special condition so that static analyzers could write the exact condition when problem happens

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
+#include <stdio.h>
 
 int main()
 {
-    int *a;
-    int *n = a;
-    a = n;
-    *n = 5;
+    int input;
+    if (scanf("%d", &input) == 1)
+    {
+        if (input == 2)
+        {
+            int *a;
+            int *n = a;
+            a = n;
+            *n = 5;
+        }
+        else
+        {
+            printf("OK\n");
+        }
+    }
     return 0;
 }
